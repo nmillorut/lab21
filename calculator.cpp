fdef2fee837e7e0e5371ea4fc5eed0e44f1abd35
nmillorut <nmillorut@gmail.com>
2025-02-21 01:27:49 +0700

    calculator.cpp completed

--- calculator.cpp
+++ calculator.cpp
@@ -1,9 +1,67 @@
 #include <windows.h>
+#include <stdlib.h>
+#include <cstdio>
 
+HWND textfield,input1,input2,Button1,Button2,Button3,Button4;
+char textSaved[100];
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
 		
+		case WM_CREATE:
+			textfield = CreateWindow("Static","     Please input two numbers",WS_VISIBLE | WS_CHILD,25,20,200,20,hwnd,NULL,NULL,NULL);
+			input1 = CreateWindow("Edit","", WS_BORDER | WS_CHILD | WS_VISIBLE,50,50,150,20,hwnd, NULL, NULL, NULL);
+			input2 = CreateWindow("Edit","", WS_BORDER | WS_CHILD | WS_VISIBLE,50,75,150,20,hwnd, NULL, NULL, NULL);
+			Button1 = CreateWindow("Button","+",WS_BORDER | WS_CHILD | WS_VISIBLE,75,120,20,20,hwnd, (HMENU) 1, NULL, NULL);
+			Button2 = CreateWindow("Button","-",WS_BORDER | WS_CHILD | WS_VISIBLE,100,120,20,20,hwnd, (HMENU) 2, NULL, NULL);
+			Button3 = CreateWindow("Button","*",WS_BORDER | WS_CHILD | WS_VISIBLE,125,120,20,20,hwnd, (HMENU) 3, NULL, NULL);
+			Button4 = CreateWindow("Button","/",WS_BORDER | WS_CHILD | WS_VISIBLE,150,120,20,20,hwnd,(HMENU) 4, NULL, NULL);
+		break;
+		case WM_PAINT: {
+            HDC hdc;
+            PAINTSTRUCT ps;
+            hdc = BeginPaint(hwnd, &ps);
+
+            HBRUSH hBrush = CreateSolidBrush(RGB(1, 255, 28));
+            RECT rect;
+            GetClientRect(hwnd, &rect);
+            FillRect(hdc, &rect, hBrush);
+
+            DeleteObject(hBrush);
+
+            EndPaint(hwnd, &ps);
+        }
+        break;
+		case WM_COMMAND:
+			switch (LOWORD(wParam))
+			{
+				case 1:
+                case 2:
+                case 3:
+                case 4:
+                    double num1, num2, result;
+
+                    GetWindowText(input1, textSaved, 100);
+                    num1 = atof(textSaved);
+
+                    GetWindowText(input2, textSaved, 100);
+                    num2 = atof(textSaved);
+
+                    switch (LOWORD(wParam)) {
+                        case 1: result = num1 + num2; break;
+                        case 2: result = num1 - num2; break;
+                        case 3: result = num1 * num2; break;
+                        case 4: if (num2 != 0) {result = num1 / num2;} 
+								else {MessageBox(hwnd, "Error", "Error", NULL);
+                                return 0;}
+                            break;
+                    }
+
+                    sprintf(textSaved, "%0.2f", result);
+                    MessageBox(hwnd, textSaved, "Result", MB_OK);
+                    break;
+			}
+		break;
 		/* Upon destruction, tell the main thread to stop */
 		case WM_DESTROY: {
 			PostQuitMessage(0);
@@ -41,11 +99,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, /* x */
-		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","My Calculator",WS_VISIBLE|WS_SYSMENU,
+		650, /* x */
+		300, /* y */
+		250, /* width */
+		200, /* height */
 		NULL,NULL,hInstance,NULL);
 
 	if(hwnd == NULL) {
